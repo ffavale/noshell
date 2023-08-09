0131209e93f4d15084b83c8a85b6ad44bd04a73e
Francesco Favale <francesco@favale.eu>
2023-08-09 18:27:00 +0200

    Started the refactoring of the noshell library

---
+++ src/lib.rs
@@ -0,0 +1,6 @@
+pub fn about() -> String {
+    let abt_str = "A simple library to run shell commands in rust programs";
+    String::from(abt_str)
+}
+
+mod tests;
---
+++ src/tests.rs
@@ -0,0 +1,7 @@
+#[cfg(test)]
+use crate::*;
+
+#[test]
+fn about_string() {
+    let _ = about();
+}
