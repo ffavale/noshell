f5135a42574f5977a5b67d3c78a73602f3f62004
Francesco Favale <francesco@favale.eu>
2023-08-10 13:49:54 +0200

    Added the ShellCommand struct like before but with an envar variable

--- src/lib.rs
+++ src/lib.rs
@@ -3,4 +3,20 @@ pub fn about() -> String {
     String::from(abt_str)
 }
 
+pub struct ShellCommand {
+    stdin: Option<String>,
+    argv: Vec<String>,
+    env_var: Vec<(String, String)>,
+}
+
+impl ShellCommand {
+    pub fn new(arg0: impl Into<String> + Copy) -> ShellCommand {
+        ShellCommand {
+            stdin: None,
+            argv: vec![arg0.into()],
+            env_var: Vec::new(),
+        }
+    }
+}
+
 mod tests;
--- src/tests.rs
+++ src/tests.rs
@@ -5,3 +5,8 @@ use crate::*;
 fn about_string() {
     let _ = about();
 }
+
+#[test]
+fn new_command() {
+    let _cmd = ShellCommand::new("ls");
+}
