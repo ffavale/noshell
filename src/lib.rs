0d2680b791e2147ed1bcd9493c218b403e60bed2
Francesco Favale <francesco@favale.eu>
2023-08-10 14:06:26 +0200

    Rapid changes and added function to add args to a ShellCommand

--- src/lib.rs
+++ src/lib.rs
@@ -1,20 +1,43 @@
+use std::collections::HashMap;
+
 pub fn about() -> String {
     let abt_str = "A simple library to run shell commands in rust programs";
     String::from(abt_str)
 }
 
+/// Shell command structure
+/// Holds all the information needed to have the OS dispatch a process
 pub struct ShellCommand {
+    env_var: Option<HashMap<String, String>>,
     stdin: Option<String>,
     argv: Vec<String>,
-    env_var: Vec<(String, String)>,
 }
 
 impl ShellCommand {
+    /// Create a new ShellCommand
     pub fn new(arg0: impl Into<String> + Copy) -> ShellCommand {
         ShellCommand {
+            env_var: None,
             stdin: None,
             argv: vec![arg0.into()],
-            env_var: Vec::new(),
+        }
+    }
+
+    /// Add arguments to a ShellCommand
+    pub fn args<I, S>(self, args: I) -> Self
+    where
+        I: IntoIterator<Item = S>,
+        S: Into<String>,
+    {
+        let arg_addition: Vec<String> = args.into_iter().map(|item: S| item.into()).collect();
+
+        let mut argv = self.argv;
+        argv.extend(arg_addition);
+
+        ShellCommand {
+            env_var: self.env_var,
+            stdin: self.stdin,
+            argv,
         }
     }
 }
